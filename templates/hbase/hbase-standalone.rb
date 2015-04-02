26bdd10814b1cc94a709ccf375addb7636e3fa2c
Роман Беляковский <ihryamzik@gmail.com>
2015-04-03 02:02:27 +0300

    added eye config for standalone version of hbase

---
+++ templates/hbase/hbase-standalone.rb
@@ -0,0 +1,19 @@
+Eye.application 'hbase-standalone' do
+  working_dir '/etc/eye'
+  stdall '/var/log/eye/hbase-standalone-stdall.log' # stdout,err logs for processes by default
+  trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
+  check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+  env "USER" => "root"
+
+  process :hbase do
+    pid_file '/tmp/hbase-root-master.pid'
+    start_command '/opt/hbase/bin/start-hbase.sh'
+    stop_command '/opt/hbase/bin/stop-hbase.sh'
+
+    daemonize true
+    start_timeout 10.seconds
+    stop_timeout 5.seconds
+
+  end
+
+end
